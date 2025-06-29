e32bdb701d120414b32536aa91c0630b15d10fb8
Your Name <you@example.com>
2025-06-29 14:02:49 +0530

    new2

---
+++ section3/springsecsection3/src/main/java/com/travelbank/TravelBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.travelbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelBankBackendApplication.class, args);
+    }
+
+}
