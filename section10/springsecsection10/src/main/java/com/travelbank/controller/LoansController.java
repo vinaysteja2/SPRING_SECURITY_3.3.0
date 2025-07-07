1e58a0f2344d8ce06dbfb7529f723fceff772030
Your Name <you@example.com>
2025-07-07 16:49:02 +0530

    added custom securityfilters

---
+++ section10/springsecsection10/src/main/java/com/travelbank/controller/LoansController.java
@@ -0,0 +1,29 @@
+package com.travelbank.controller;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.travelbank.model.Loans;
+import com.travelbank.repository.LoanRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class LoansController {
+
+    private final LoanRepository loanRepository;
+
+    @GetMapping("/myLoans")
+    public List<Loans> getLoanDetails(@RequestParam long id) {
+        List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
+        if (loans != null) {
+            return loans;
+        } else {
+            return null;
+        }
+    }
+
+}
